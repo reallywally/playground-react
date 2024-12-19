b2ec78ee7f4fce97bb627e68b63db31efdb1efcb
reallywally <goldenale5500@gmail.com>
2024-12-19 23:54:27 +0900

    props 테스트트

--- src/playground/propsTest/Parent.tsx
+++ src/playground/propsTest/Parent.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Child1 from "./Child1";
 
-export default function Parent() {
-  console.log("Parent rendered");
-  const data = [
+const ParentComponent: React.FC = () => {
+  const users = [
     { id: 1, name: "John", age: 25 },
     { id: 2, name: "Doe", age: 30 },
   ];
 
   return (
     <div>
-      Parent
-      <Child1 data={data} />
+      <h1>Parent Component</h1>
+      <Child1 users={users} />
     </div>
   );
-}
+};
+
+export default ParentComponent;
