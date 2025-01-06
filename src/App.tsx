7e0464852b2b2cc3088eda61e0874c9ec10fa604
reallywally <goldenale5500@gmail.com>
2025-01-06 14:53:38 +0900

    props 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import Layout from "./components/Layout";
-import Parent from "./playground/useEffectTest/Parent";
+import Parent from "./playground/propsTest/Parent";
 import JsonEditor from "./playground/jsonEditorTest/JsonEditor";
 import JsonEditor2 from "./playground/jsonEditorTest/JsonEditor2";
 
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
-          <Route path="/playground" element={<Parent />} />
+          <Route path="/props-test" element={<Parent />} />
           <Route path="/json-editor" element={<JsonEditor2 />} />
         </Routes>
       </Layout>
--- src/playground/propsTest/Parent.tsx
+++ src/playground/propsTest/Parent.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Child1 from "./Child1";
 
 const ParentComponent: React.FC = () => {
-  const users = [
+  const initData = [
     { id: 1, name: "John", age: 25 },
     { id: 2, name: "Doe", age: 30 },
   ];
+  const [users, setUsers] = useState(initData);
 
   return (
     <div>
       <h1>Parent Component</h1>
+      <button
+        onClick={() => setUsers([...users, { id: 3, name: "Jane", age: 22 }])}
+      >
+        add user
+      </button>
       <Child1 users={users} />
     </div>
   );
