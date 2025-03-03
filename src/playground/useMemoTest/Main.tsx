fb58aa28e86009ceac10d1b26bdda5195a8f0d3b
reallywally <goldenale5500@gmail.com>
2025-03-03 23:38:08 +0900

    useMemo 테스트 코드 작성 중

---
+++ src/playground/useMemoTest/Main.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useMemo, useState } from "react";
+
+function ExpensiveComponent({ value }) {
+  useEffect(() => {
+    console.log("rendered");
+  });
+  return <div>{value + 100}</div>;
+}
+
+const MemoTest = () => {
+  const [value, setValue] = useState(0);
+  const [, triggerRendering] = useState(false);
+
+  const MemoizedComponent = useMemo(
+    () => <ExpensiveComponent value={value} />,
+    [value]
+  );
+
+  const handleCange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    triggerRendering((prev) => !prev);
+  };
+
+  return (
+    <>
+      <input value={value} onChange={handleCange} />
+      <button onClick={handleClick}>Re-render</button>
+      {MemoizedComponent}
+    </>
+  );
+};
+
+export default MemoTest;
